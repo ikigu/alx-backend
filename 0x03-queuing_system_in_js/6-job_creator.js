15bbe333552b79d45a90bcd9ec51518705d88ad5
George Ikigu <ikigu.geo@gmail.com>
2024-09-26 04:03:54 +0300

    solutions: task 0-9

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,20 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue({name: 'push_notification_code'});
+
+const job = queue.create('push_notification_code', {
+  phoneNumber: '0797049495',
+  message: 'This is your verification code: 098-987',
+});
+
+job
+  .on('enqueue', () => {
+    console.log('Notification job created:', job.id);
+  })
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed attempt', () => {
+    console.log('Notification job failed');
+  });
+job.save();
